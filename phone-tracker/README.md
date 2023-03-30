6d524c48d2e9383713e0eebaea0c28353e1ccd88
Marcel Stancu <marcelstancu12@yahoo.com>
2023-03-30 11:03:37 +0300

    Added phone tracker project

---
+++ phone-tracker/README.md
@@ -0,0 +1,13 @@
+## Instalation
+
+1. In order to use this project, you need to install those python libraries:
+- pip install phonenumbers
+- pip install opencage
+- pip install folium
+
+2. In the file "myphone.py", you need to specify your phone number.
+
+3. In the file "main.py", you need to specify the API key from OpenCageData.
+In order to have an API key, you need to register to the following website: https://opencagedata.com/ 
+
+4. Open file "myLocation.html" using your main browser.
---
+++ phone-tracker/main.py
@@ -0,0 +1,30 @@
+from opencage.geocoder import OpenCageGeocode
+from phonenumbers import carrier
+import phonenumbers
+import opencage
+import folium
+from myphone import number
+
+from phonenumbers import geocoder
+
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+service_pro = phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, "en"))
+
+
+key = str("KEY HERE")
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+
+myMap = folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to(myMap)
+
+myMap.save("myLocation.html")
